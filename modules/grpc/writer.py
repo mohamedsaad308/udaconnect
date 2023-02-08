07488590922c79c39f4027eeb3e159de6bcb316d
mohamedsaad308 <mohamedsaad308@gmail.com>
2023-02-08 12:09:56 +0200

    update grpc readme and writer server

--- modules/grpc/writer.py
+++ modules/grpc/writer.py
@@ -5,7 +5,7 @@ import location_pb2_grpc
 print("Sending sample payload...")
 
 
-channel = grpc.insecure_channel("localhost:5005")
+channel = grpc.insecure_channel("localhost:30020")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
 location = location_pb2.LocationMessage(
